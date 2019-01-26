d08aa81923e5a7ed054a76fc07701033efc592ac
Richard <einetunet@gmail.com>
2019-01-26 19:47:32 +0100

    Test

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
 var userArray = [];
-var totalUserCount = 0;
 app.use('/',express.static('build/public'))
  
 console.log('HELLO');
 io.on('connection', (socket) =>{
   console.log('a user is connected')
 
-    totalUserCount = io.engine.clientsCount;
     
  
  });
@@ -21,7 +19,7 @@ io.on('connection', (socket) =>{
   setInterval(sendToAllconnectedClients, 33);
 
  function sendToAllconnectedClients() {
-   io.emit('state' , {"date": Date.now(), "userCount": totalUserCount});
+   io.emit('state' , {"date": Date.now(), "userCount": io.engine.clientsCount});
  }
 
 
